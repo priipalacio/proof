f4358e375a3c8f58e23a780e6d8c800d359dd249
priipalacio <54598457+priipalacio@users.noreply.github.com>
2025-08-19 17:02:10 -0300

    add hook

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,54 @@
+#
+//  post_gen_project.sh
+//  proof
+//
+//  Created by Pri Palacio on 19/08/2025.
+//
+
+#!/bin/bash
+set -e
+
+PROJECT_DIR="$(pwd)"
+APP_NAME="{{ cookiecutter.project_name }}"
+DEMO_NAME="${APP_NAME}Demo"
+
+mkdir -p "$PROJECT_DIR/Demo/$DEMO_NAME"
+
+# Crear un project.yml dinámico
+cat > "$PROJECT_DIR/Demo/project.yml" <<EOF
+name: $DEMO_NAME
+options:
+  bundleIdPrefix: com.example
+targets:
+  $DEMO_NAME:
+    type: application
+    platform: iOS
+    sources:
+      - $DEMO_NAME
+    scheme:
+      testTargets:
+        - ${APP_NAME}DemoTests
+  ${APP_NAME}DemoTests:
+    type: bundle.unit-test
+    platform: iOS
+    sources:
+      - ${APP_NAME}DemoTests
+    dependencies:
+      - target: $DEMO_NAME
+  ${APP_NAME}DemoUITests:
+    type: bundle.ui-testing
+    platform: iOS
+    sources:
+      - ${APP_NAME}DemoUITests
+    dependencies:
+      - target: $DEMO_NAME
+EOF
+
+# Crear carpetas de código
+mkdir -p "$PROJECT_DIR/Demo/$DEMO_NAME"
+mkdir -p "$PROJECT_DIR/Demo/${APP_NAME}DemoTests"
+mkdir -p "$PROJECT_DIR/Demo/${APP_NAME}DemoUITests"
+
+# Generar el Xcode project
+cd "$PROJECT_DIR/Demo"
+xcodegen generate
