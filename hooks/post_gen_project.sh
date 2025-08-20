153c178bdbe95883f3dc6923a8f08b4ecf8f9bab
priipalacio <54598457+priipalacio@users.noreply.github.com>
2025-08-20 16:11:37 -0300

    up

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,11 +1,11 @@
-##!/bin/sh
-#set -e  # parar si hay error
-#
-## Ruta raíz del proyecto generado
-#PROJECT_DIR="$(pwd)"
-#
-## Nombre del proyecto (cookiecutter lo reemplaza antes de ejecutar este hook)
-#APP_NAME="{{cookiecutter.project_name}}"
-#DEMO_NAME="${APP_NAME}Demo"
-#
-#replace "com.company.PRODUCTNAME" "{{cookiecutter.bundle_identifier }}"
+#!/bin/sh
+set -e  # parar si hay error
+
+# Ruta raíz del proyecto generado
+PROJECT_DIR="$(pwd)"
+
+# Nombre del proyecto (cookiecutter lo reemplaza antes de ejecutar este hook)
+APP_NAME="{{cookiecutter.project_name}}Demo"
+DEMO_NAME="${APP_NAME}Demo"
+
+mv "$PROJECT_DIR/$APPNAME" "$PROJECT_DIR/Demo"
