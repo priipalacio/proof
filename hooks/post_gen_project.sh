458d5971ce5fe3b4a0f994691b0adf28a26db093
priipalacio <54598457+priipalacio@users.noreply.github.com>
2025-08-20 16:05:23 -0300

    fix

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,10 +5,8 @@ set -e  # parar si hay error
 PROJECT_DIR="$(pwd)"
 
 # Nombre del proyecto (cookiecutter lo reemplaza antes de ejecutar este hook)
-APP_NAME="{{ cookiecutter.project_name }}"
+APP_NAME="{{cookiecutter.project_name}}"
 DEMO_NAME="${APP_NAME}Demo"
-
-mv "$PROJECT_DIR/$APP_NAMEDemo" "$PROJECT_DIR/Demo/"
 
 replace "ORGANIZATION" "{{ cookiecutter.company_name }}"
 replace "com.company.PRODUCTNAME" "{{ cookiecutter.bundle_identifier }}"
