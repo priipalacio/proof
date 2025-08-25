18a11ae8f0f14622840d218caf54ce28e7f10bae
priipalacio <54598457+priipalacio@users.noreply.github.com>
2025-08-25 16:39:28 -0300

    updated

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,6 +4,47 @@ set -e
 # Ruta raíz del proyecto generado
 PROJECT_DIR="../"
 
+cd "$PROJECT_DIR"
+
+cat > "$PROJECT_DIR/project.yml"<<EOF
+name: {{cookiecutter.project_name}}Demo
+options:
+  minimumXcodeGenVersion: 2.0.0
+  deploymentTarget:
+    iOS: "15.0"
+
+packages:
+  {{cookiecutter.project_name}}:
+    path: ../../{{cookiecutter.repository_name}}
+
+targets:
+  {{cookiecutter.project_name}}Demo:
+    type: application
+    platform: iOS
+    bundleId: com.uala.{{cookiecutter.bundle_identifier}}
+    sources:
+      - path: {{cookiecutter.project_name}}Demo
+        exclude:
+         - project.yml
+    dependencies:
+      - package: {{cookiecutter.project_name}}
+
+  {{cookiecutter.project_name}}DemoTests:
+    type: bundle.unit-test
+    platform: iOS
+    sources:
+      - path: {{cookiecutter.project_name}}DemoTests
+    dependencies:
+      - target: {{cookiecutter.project_name}}Demo
+
+  {{cookiecutter.project_name}}DemoUITests:
+    type: bundle.ui-testing
+    platform: iOS
+    sources:
+      - path: {{cookiecutter.project_name}}DemoUITests
+    dependencies:
+      - target: {{cookiecutter.project_name}}Demo
+EOF
 
 echo "# Changelog" > CHANGELOG.md
 echo "" >> CHANGELOG.md
