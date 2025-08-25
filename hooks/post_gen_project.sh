02d3e6c76b763f424017235ee0b5eec573266fd3
priipalacio <54598457+priipalacio@users.noreply.github.com>
2025-08-25 16:34:57 -0300

    update template

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,3 +5,15 @@ set -e
 PROJECT_DIR="../"
 
 
+echo "# Changelog" > CHANGELOG.md
+echo "" >> CHANGELOG.md
+echo "All notable changes to this project will be documented in this file." >> CHANGELOG.md
+echo "" >> CHANGELOG.md
+echo "## [Unreleased]" >> CHANGELOG.md
+
+if command -v xcodegen >/dev/null 2>&1; then
+  xcodegen generate
+else
+  echo "⚠️ XcodeGen no está instalado. Instalalo con: brew install xcodegen"
+  exit 1
+fi
---
+++ {{cookiecutter.project_name}}/Sources/MainModule.swift
@@ -0,0 +1,7 @@
+//
+//  MainModule.swift
+//  proof
+//
+//  Created by Pri Palacio on 25/08/2025.
+//
+
