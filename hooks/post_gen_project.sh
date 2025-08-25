11f7c1e05a62aa5280885f5dad668b2f04cc2a1b
priipalacio <54598457+priipalacio@users.noreply.github.com>
2025-08-25 17:31:45 -0300

    y

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -11,6 +11,61 @@ mkdir -p ../Demo/{{cookiecutter.project_name}}DemoTests
 mkdir -p ../Demo/{{cookiecutter.project_name}}DemoUITests
 
 cd ../Demo
+
+# App principal
+cat > "../Demo/${DEMO_NAME}Demo/${DEMO_NAME}App.swift" <<EOF
+import SwiftUI
+
+@main
+struct ${DEMO_NAME}App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+EOF
+
+# ContentView
+cat > "../Demo/${DEMO_NAME}Demo/ContentView.swift" <<EOF
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello from ${DEMO_NAME}!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
+EOF
+
+cat > "../Demo/${DEMO_NAME}DemoTests/${DEMO_NAME}DemoTests.swift" <<EOF
+import XCTest
+@testable import ${DEMO_NAME}
+
+final class ${APP_NAME}DemoTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
+EOF
+
+cat > "../Demo/${DEMO_NAME}DemoUITests/${DEMO_NAME}DemoUITests.swift" <<EOF
+import XCTest
+
+final class ${APP_NAME}DemoUITests: XCTestCase {
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+        XCTAssertTrue(app.buttons.count >= 0)
+    }
+}
+EOF
+
+
+cd ../Demo
 cat > "project.yml"<<EOF
 name: {{cookiecutter.project_name}}Demo
 options:
@@ -55,6 +110,14 @@ targets:
       - target: {{cookiecutter.project_name}}Demo
 EOF
 
+if command -v xcodegen >/dev/null 2>&1; then
+  xcodegen generate
+  rm project.yml
+else
+  echo "⚠️ XcodeGen no está instalado. Instalalo con: brew install xcodegen"
+  exit 1
+fi
+
 cd ..
 echo "# Changelog" > CHANGELOG.md
 echo "" >> CHANGELOG.md
@@ -91,63 +154,3 @@ let package = Package(
     ]
 )
 EOF
-
-# App principal
-cat > "./Demo/${DEMO_NAME}Demo/${DEMO_NAME}App.swift" <<EOF
-import SwiftUI
-
-@main
-struct ${DEMO_NAME}App: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
-EOF
-
-# ContentView
-cat > "Demo/${DEMO_NAME}Demo/ContentView.swift" <<EOF
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        Text("Hello from ${DEMO_NAME}!")
-    }
-}
-
-#Preview {
-    ContentView()
-}
-EOF
-
-cat > "Demo/${DEMO_NAME}DemoTests/${DEMO_NAME}DemoTests.swift" <<EOF
-import XCTest
-@testable import ${DEMO_NAME}
-
-final class ${APP_NAME}DemoTests: XCTestCase {
-    func testExample() throws {
-        XCTAssertTrue(true)
-    }
-}
-EOF
-
-cat > "Demo/${DEMO_NAME}DemoUITests/${DEMO_NAME}DemoUITests.swift" <<EOF
-import XCTest
-
-final class ${APP_NAME}DemoUITests: XCTestCase {
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-        XCTAssertTrue(app.buttons.count >= 0)
-    }
-}
-EOF
-
-if command -v xcodegen >/dev/null 2>&1; then
-  xcodegen generate
-  rm project.yml
-else
-  echo "⚠️ XcodeGen no está instalado. Instalalo con: brew install xcodegen"
-  exit 1
-fi
